5bdf20b9de913f865af8130bb417979c5cfedbda
Liang Mi <github@liangmi.dev>
2026-06-09 21:50:15 +0800

    feat

---
+++ src/base/fmt.ts
@@ -0,0 +1,8 @@
+import type { OxfmtConfig } from "vite-plus/fmt";
+
+export const fmt: OxfmtConfig = {
+  arrowParens: "avoid",
+  embeddedLanguageFormatting: "off",
+  jsxSingleQuote: true,
+  proseWrap: "never",
+};

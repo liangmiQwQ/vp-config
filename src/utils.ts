922f083a9980739a3a079122faafa4124ae27bfa
Liang Mi <github@liangmi.dev>
2026-06-11 06:08:11 +0800

    wip

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+/**
+ * Check whether the config is a workspace root config or a nested config.
+ */
+export function isWorkspaceRoot(): boolean {
+  // 1. Check if there is pnpm-workspace.yaml in the directory, return true if there is.
+  // 2. Check the parent directory
+  return false
+}

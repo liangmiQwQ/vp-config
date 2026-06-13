6fe6beaf4a5860f48355a780814292bed33c77b5
Liang Mi <github@liangmi.dev>
2026-06-13 17:31:23 +0800

    wip

--- src/website/lint.ts
+++ src/website/lint.ts
@@ -5,5 +5,13 @@ import { lintBase } from '../base/lint.ts'
 
 // This is incomplete, Oxlint's better Vue support (template support) is needed since I personally mainly develop website with Vue.js
 export const lintWebsite: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(lintBase, {
-  env: { vue: true, browser: true }
+  env: { vue: true, browser: true },
+  rules: {
+    'prefer-dom-node-append': 'warn',
+    'prefer-dom-node-dataset': 'warn',
+    'unicorn/prefer-query-selector': 'warn',
+
+    'prefer-dom-node-remove': 'warn',
+    'prefer-blob-reading-methods': 'warn'
+  }
 })

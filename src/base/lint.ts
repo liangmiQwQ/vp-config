abe78533ba322c3112e27c63c0712bc06e5d746f
Liang Mi <github@liangmi.dev>
2026-06-13 15:07:18 +0800

    wip

--- src/base/lint.ts
+++ src/base/lint.ts
@@ -10,22 +10,61 @@ const nursery: DummyRuleMap = {
   'no-restricted-exports': 'off'
 }
 
+// Whitelist mode
+const pedantic: DummyRuleMap = {
+  eqeqeq: 'error',
+  'no-self-compare': 'error',
+  'oxc/branches-sharing-code': 'error',
+  'array-callback-return': 'error',
+  'unicorn/consistent-empty-array-spread': 'error',
+  // We hope not to use classes, if there is a case we have to use, just use oxlint-disable to disable it.
+  'max-classes-per-file': ['error', { max: 0 }],
+  'max-depth': ['error', { max: 5 }],
+  'eslint/max-nested-callbacks': ['error', { max: 6 }],
+  'unicorn/new-for-builtins': 'error', // It is similar to `no-new-wrappers`, prefer unicorn/new-for-builtins here.
+  'no-case-declarations': 'error',
+  'no-constructor-return': 'error',
+  'typescript/no-deprecated': 'error',
+  'no-fallthrough': 'error',
+  'unicorn/no-immediate-mutation': 'error',
+  'no-loop-func': 'error',
+  'typescript/no-misused-promises': 'error',
+  'typescript/no-mixed-enums': 'error',
+  'unicorn/no-new-buffer': 'error',
+  'no-object-constructor': 'error',
+  'no-promise-executor-return': 'error',
+
+  // Rules are reported as `warn`. Most of them are fixable, mainly about readability and style.
+  'unicorn/no-hex-escape': 'warn',
+  'unicorn/no-instanceof-array': 'warn',
+  'no-else-return': 'warn',
+  'typescript/ban-ts-comment': 'warn',
+  'typescript/no-confusing-void-expression': 'warn',
+  'unicorn/escape-case': 'warn',
+  'unicorn/explicit-length-check': 'warn',
+  'no-array-constructor': 'warn',
+
+  'no-lonely-if': 'warn',
+  'unicorn/no-negation-in-equality-check': 'warn',
+  'no-negated-condition': 'warn'
+}
+
 export const lintBase: OxlintConfig = {
   categories: {
     correctness: 'error',
     perf: 'error',
-    restriction: 'error',
     suspicious: 'error',
     nursery: 'error',
-    pedantic: 'warn',
+
+    // White list mode
+    restriction: 'off',
+    pedantic: 'off',
     style: 'off'
   },
   plugins: ['eslint', 'oxc', 'import', 'promise', 'typescript', 'unicorn'],
   rules: {
-    // oxlint-disable-next-line oxc/no-rest-spread-properties
     ...nursery,
-
-    'no-inline-comments': 'off'
+    ...pedantic
   },
   options: {
     typeAware: true,

dbc53118343f57dbfaacb2d395e6f0dae1fb855b
Liang Mi <github@liangmi.dev>
2026-06-14 11:26:59 +0800

    enable rules

--- src/shared/lint.ts
+++ src/shared/lint.ts
@@ -1,5 +1,8 @@
 import type { OxlintConfig } from 'vite-plus/lint'
 
+// Some lint shard override.
+// They should be used in combination mostly.
+
 // For code that's sure running on Node.js, mainly for CLI use.
 export const cliOverride: OxlintConfig = {
   env: { node: true },
@@ -10,4 +13,10 @@ export const cliOverride: OxlintConfig = {
     'unicorn/no-process-exit': 'off',
     'node/no-path-concat': 'error'
   }
+}
+
+// For React and Vue component. There is no web-specific or node-specific rules so it can be used for both cli and website
+export const componentOverride: OxlintConfig = {
+  env: { vue: true },
+  plugins: ['jsx-a11y', 'react', 'react-perf', 'vue']
 }
--- src/website/lint.ts
+++ src/website/lint.ts
@@ -5,7 +5,7 @@ import { lintBase } from '../base/lint.ts'
 
 // This is incomplete, Oxlint's better Vue support (template support) is needed since I personally mainly develop website with Vue.js
 export const lintWebsite: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(lintBase, {
-  env: { vue: true, browser: true },
+  env: { browser: true },
   rules: {
     'prefer-dom-node-append': 'warn',
     'prefer-dom-node-dataset': 'warn',

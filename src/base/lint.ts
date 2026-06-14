53d867264e01e651b0cc838877df52c25dd037ee
Liang Mi <github@liangmi.dev>
2026-06-15 05:52:07 +0800

    wip

--- src/base/lint.ts
+++ src/base/lint.ts
@@ -4,7 +4,8 @@ import { cliOverride } from '../shared/lint.ts'
 
 // Rules Config
 const suspicious: DummyRuleMap = {
-  'no-shadow': 'off'
+  'no-shadow': 'off',
+  'typescript/no-unsafe-type-assertion': 'off'
 }
 
 const nursery: DummyRuleMap = {
@@ -191,13 +192,14 @@ export const lintBase: OxlintConfig = {
     ...restriction,
     ...pedantic,
     ...style,
+
+    // `liangmi` plugin
     'liangmi/no-orphan-vite-config': 'error',
     'liangmi/no-useless-vp-preset-imports': 'error',
     'liangmi/use-preset-vp-config': 'error',
     'liangmi/load-proper-vp-config-category': 'error',
     'liangmi/no-mixed-project': 'error',
-    'liangmi/cleanup': 'error',
-    'typescript/no-unsafe-type-assertion': 'off'
+    'liangmi/cleanup': 'warn' // It should not be report
   },
   options: {
     typeAware: true,

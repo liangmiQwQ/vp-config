a2737d93960a5c34b7a1c3f9f5a8c92feb382c43
Liang Mi <github@liangmi.dev>
2026-06-14 11:19:19 +0800

    chore: adjust lint config for cli

--- src/shared/lint.ts
+++ src/shared/lint.ts
@@ -1,7 +1,9 @@
 import type { OxlintConfig } from 'vite-plus/lint'
 
+// For code that's sure running on Node.js, mainly for CLI use.
 export const cliOverride: OxlintConfig = {
   env: { node: true },
+  // We don't enable most of the rules in this plugin, just follow the configure in lintBase.
   plugins: ['node'],
   rules: {
     'no-console': 'off',

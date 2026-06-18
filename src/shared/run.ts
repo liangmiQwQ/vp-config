2b3d832670e736d0772965fb1e1ad3532e8cf893
Liang Mi <github@liangmi.dev>
2026-06-19 06:49:39 +0800

    adjust

--- src/shared/run.ts
+++ src/shared/run.ts
@@ -3,10 +3,10 @@ import type { UserConfig } from 'vite-plus'
 export const run: NonNullable<UserConfig['run']> = {
   tasks: {
     cbuild: 'vp build',
-    ccheck: 'vp check',
+    ccheck: { command: 'vp check', input: ['!node_modules/.vp-config/info.json'] },
     cfmt: 'vp fmt',
     cformat: 'vp format',
-    clint: 'vp lint',
+    clint: { command: 'vp lint', input: ['!node_modules/.vp-config/info.json'] },
     cpack: 'vp pack',
     ctest: 'vp test'
   }

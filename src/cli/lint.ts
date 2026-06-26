61131250c530b81e873332a806527e5e5a74b469
Liang <github@liangmi.dev>
2026-06-26 10:07:30 +0800

    feat(lint): ignore bin wrapper files (#35)
    Bin wrappers can have special names, inside can have wrapper code, we
    can just ignore them.

--- src/cli/lint.ts
+++ src/cli/lint.ts
@@ -4,16 +4,4 @@ import type { OxlintConfig } from 'vite-plus/lint'
 import { lintBase } from '../base/lint.ts'
 import { tuiOverride } from '../shared/lint.ts'
 
-export const lintCli: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(
-  lintBase,
-  mergeConfig<OxlintConfig, OxlintConfig>(tuiOverride, {
-    overrides: [
-      {
-        rules: {
-          'import/no-unassigned-import': 'off'
-        },
-        files: ['bin/**']
-      }
-    ]
-  })
-)
+export const lintCli: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(lintBase, tuiOverride)

1fed0efb3b159fce79397e53b4a2bb74ef10b555
Liang Mi <github@liangmi.dev>
2026-06-26 10:06:04 +0800

    a

--- src/cli/lint.ts
+++ src/cli/lint.ts
@@ -4,7 +4,4 @@ import type { OxlintConfig } from 'vite-plus/lint'
 import { lintBase } from '../base/lint.ts'
 import { tuiOverride } from '../shared/lint.ts'
 
-export const lintCli: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(
-  lintBase,
-  tuiOverride
-)
+export const lintCli: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(lintBase, tuiOverride)

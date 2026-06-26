0b68264646b090a7ae7af9064bc4a7f86e47318e
Liang Mi <github@liangmi.dev>
2026-06-26 10:03:45 +0800

    wip

--- src/cli/lint.ts
+++ src/cli/lint.ts
@@ -6,17 +6,5 @@ import { cliOverride, componentOverride } from '../shared/lint.ts'
 
 export const lintCli: OxlintConfig = mergeConfig<OxlintConfig, OxlintConfig>(
   lintBase,
-  mergeConfig<OxlintConfig, OxlintConfig>(
-    mergeConfig<OxlintConfig, OxlintConfig>(cliOverride, componentOverride),
-    {
-      overrides: [
-        {
-          rules: {
-            'import/no-unassigned-import': 'off'
-          },
-          files: ['bin/**']
-        }
-      ]
-    }
-  )
+  mergeConfig<OxlintConfig, OxlintConfig>(cliOverride, componentOverride)
 )
